2a3f2d80998b977054755742a66154b1aa3d6c80
Andre Rodriguez <drae7299@gmail.com>
2021-05-20 12:42:26 -0400

    added in more server consts and npm packages, adding in cotroller next

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const routes = require("./controllers");
+// const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 
 const compression = require('compression')
 
 // helper function
-const helpers = require("./utils/helpers");
+// const helpers = require("./utils/helpers");
 
 // handlebars
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 // session connection to sequelize database
 const session = require("express-session");
